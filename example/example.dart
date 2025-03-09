d9aaeba80cfc7c1edfe975890c6763cad241f949
Shashi <shashi@Admins-MacBook-Pro-2.local>
2025-03-09 09:16:45 +0530

    Bumped up the http package version

--- example/example.dart
+++ example/example.dart
@@ -2,7 +2,7 @@ import 'package:bytes_image_viewer/bytes_image_viewer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const HomeScreen());
+  runApp(const MaterialApp(home: HomeScreen()));
 }
 
 class HomeScreen extends StatefulWidget {
@@ -18,19 +18,24 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: const Center(
           child: BytesImageViewer(
-        height: 100, //optional
-        width: 100, //optional
-        fit: BoxFit.cover, //optional
-        apiUrl:
+            height: double.infinity,
+            //optional
+            width: double.infinity,
+            //optional
+            fit: BoxFit.fill,
+            //optional
+            apiUrl:
             'https://cdn.pixabay.com/photo/2023/11/26/19/09/butterfly-8414148_1280.jpg',
-        headers: {"Authorization": "Bearer yTiIsInN1YiI6IjEiLC"}, //optional
-        loadingWidget: CircularProgressIndicator(
-          color: Colors.red,
-        ), //optional
-        errorWidget: CircularProgressIndicator(
-          color: Colors.red,
-        ), //optional
-      )),
+            headers: {"Authorization": "Bearer yTiIsInN1YiI6IjEiLC"},
+            //optional
+            loadingWidget: CircularProgressIndicator(
+              color: Colors.red,
+            ),
+            //optional
+            errorWidget: CircularProgressIndicator(
+              color: Colors.red,
+            ), //optional
+          )),
       appBar: AppBar(
         title: const Text("Bytes image viewer example"),
       ),
